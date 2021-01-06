532dc9e9a625d517a9ae6ccf58f25639300f0fae
imoe2010@163.com <imoe2010@163.com>
2021-01-07 00:49:13 +0800

    update

--- action.js
+++ action.js
@@ -1,4 +1,6 @@
 const TOOL = require('./tool');
+const SKILL = require('./skill');
+const BUFF = require('./buff');
 
 function createAction(curUnit, target, actionType) {
   let type = typeof actionType === 'function' ? actionType() : actionType;
@@ -19,6 +21,65 @@ function orderAction(units) {
   return units.sort(compare);
 }
 
+
+const handleAction = (action, status) => {
+  if (status.win) return '战斗已结束，行动终止';
+  let { curUnit, target } = action;
+  let results = null;
+  let logs = [`第${status.roundTime}回合：`];
+  let unitsAlias = {
+    'SELF': curUnit,
+    'TARGET':  target
+  }
+
+  curUnit.action =  true;
+
+  // 状态处理
+  curUnit.buffs.forEach(buff => {
+    let results = BUFF[buff.code].handler(curUnit, target);
+    results.forEach(result => {
+      typeof result.effectValue === 'boolean' 
+        ? unitsAlias[result.effectTarget][result.effectProp] = result.effectValue
+        : unitsAlias[result.effectTarget][result.effectProp] += (result.effectValue * (result.positive ? 1 : -1));
+    })
+  })
+
+  if(!!curUnit.action) {
+    // 操作指令
+    results = SKILL[action.type].handler(curUnit, target);
+    results.forEach((result) => {
+      let { effectProp } = result;
+      if(effectProp === 'buffs') {
+        let pro = TOOL.RandomNumBoth(100);
+        if(pro <= result.probability) {
+          unitsAlias[result.effectTarget][result.effectProp].push(result.buff);
+          logs.push(result.log)
+        }
+      } else {
+        unitsAlias[result.effectTarget][result.effectProp] += (result.effectValue * (result.positive ? 1 : -1));
+        logs.push(result.log);
+      }
+    })
+  } else {
+    logs.push(`${curUnit.name}无法行动`)
+  }
+
+  let buffcount = curUnit.buffs.length;
+  while(buffcount--) {
+    curUnit.buffs[buffcount].continued--;
+    if(curUnit.buffs[buffcount].continued <= 0) {
+      curUnit.buffs.splice(buffcount, 1);
+    }
+  }
+
+  
+  if (target.hp <= 0) {
+    logs.push(`${curUnit.name}击败了${target.name}`, JSON.stringify(status.units));
+    status.win = true;
+  }
+  return logs;
+}
+
 module.exports = {
-  createAction, orderAction
+  createAction, orderAction, handleAction
 };
--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-const SKILL = require('./skill');
 const TOOL = require('./tool');
 const ACTION = require('./action');
-const BUFF = require('./buff')
 
 let unit1 = {
   name: 'A',
@@ -11,7 +9,7 @@ let unit1 = {
   def: 5,
   spd: 10,
   type: function() {
-    return ['ATTACK', 'DOUBLE_ATTACK', 'HEMOPHAGIA', 'FIGHTING_AND_BREVE', 'HEAVY_ATTACK'][TOOL.RandomNumBoth(3)]
+    return ['ATTACK', 'DOUBLE_ATTACK', 'HEMOPHAGIA', 'FIGHTING_AND_BREVE', 'HEAVY_ATTACK'][TOOL.RandomNumBoth(4)]
   },
   id: 1,
   target: 2,
@@ -24,9 +22,9 @@ let unit2 = {
   mp: 15,
   atk: 10,
   def: 5,
-  spd: 10,
+  spd: 11,
   type: function() {
-    return ['ATTACK', 'DOUBLE_ATTACK', 'HEMOPHAGIA', 'FIGHTING_AND_BREVE', 'HEAVY_ATTACK'][TOOL.RandomNumBoth(3)]
+    return ['ATTACK', 'DOUBLE_ATTACK', 'HEMOPHAGIA', 'FIGHTING_AND_BREVE', 'HEAVY_ATTACK'][TOOL.RandomNumBoth(4)]
   },
   id: 2,
   target: 1,
@@ -34,95 +32,39 @@ let unit2 = {
   buffs: []
 };
 
-let win = false;
-let roundTime = 1;
 
-let units = {
-  1: unit1,
-  2: unit2
+let status = {
+  win: false,
+  roundTime: 1,
+  units: {
+    1: unit1,
+    2: unit2
+  }
 }
 
-while (!win) {
-  console.log(round())
+while (!status.win) {
+  console.log(round(status))
 }
 
 /**
  * 读取用户信息及指令并且返回新的用户信息
  */
 
-function round() {
+function round(status) {
 
-  if (win) return;
+  if (status.win) return;
 
   let logs = [];
 
-  let actions = ACTION.orderAction(TOOL.obj2arr(units)).map(item => {
-    return ACTION.createAction(item, units[item.target], item.type);
+  let actions = ACTION.orderAction(TOOL.obj2arr(status.units)).map(item => {
+    return ACTION.createAction(item, status.units[item.target], item.type);
   })
-
-  const handleAction = (action) => {
-    if (win) return '战斗已结束，行动终止';
-    let { curUnit, target } = action;
-    let results = null;
-    let logs = [`第${roundTime}回合：`];
-    let unitsAlias = {
-      'SELF': curUnit,
-      'TARGET':  target
-    }
-
-    curUnit.action =  true;
-
-    // 状态处理
-    curUnit.buffs.forEach(buff => {
-      let results = BUFF[buff.code].handler(curUnit, target);
-      results.forEach(result => {
-        typeof result.effectValue === 'boolean' 
-          ? unitsAlias[result.effectTarget][result.effectProp] = result.effectValue
-          : unitsAlias[result.effectTarget][result.effectProp] += (result.effectValue * (result.positive ? 1 : -1));
-      })
-    })
-
-    if(!!curUnit.action) {
-      // 操作指令
-      results = SKILL[action.type].handler(curUnit, target);
-      results.forEach((result) => {
-        let { effectProp } = result;
-        if(effectProp === 'buffs') {
-          let pro = TOOL.RandomNumBoth(100);
-          if(pro <= result.probability) {
-            unitsAlias[result.effectTarget][result.effectProp].push(result.buff);
-            logs.push(result.log)
-          }
-        } else {
-          unitsAlias[result.effectTarget][result.effectProp] += (result.effectValue * (result.positive ? 1 : -1));
-          logs.push(result.log);
-        }
-      })
-    } else {
-      logs.push(`${curUnit.name}无法行动`)
-    }
-
-    let buffcount = curUnit.buffs.length;
-    while(buffcount--) {
-      curUnit.buffs[buffcount].continued--;
-      if(curUnit.buffs[buffcount].continued <= 0) {
-        curUnit.buffs.splice(buffcount, 1);
-      }
-    }
-
-    
-    if (target.hp <= 0) {
-      logs.push(`${curUnit.name}击败了${target.name}`, JSON.stringify(unit1), JSON.stringify(unit2));
-      win = true;
-    }
-    return logs;
-  }
 
   actions.forEach(action => {
-    logs.push(handleAction(action));
+    logs.push(ACTION.handleAction(action, status));
   })
 
-  roundTime++;
+  status.roundTime++;
 
   return { logs }
 }
